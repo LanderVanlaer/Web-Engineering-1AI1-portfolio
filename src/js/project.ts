63bde5a2b915ad8a8af31edd71d6d7f66d8c7d2a
Lander Van laer <lander.vanlaer1@gmail.com>
2022-05-10 15:19:12 +0200

    add slideshow project page

--- src/js/project.ts
+++ src/js/project.ts
@@ -9,4 +9,22 @@ $(() => {
   for (let i = 2; i <= 5; ++i) {
     $(`#poster > div:nth-child(${i})`).fadeIn(i * 1750);
   }
+
+  const slideShow: { images: NodeListOf<HTMLImageElement>; index: number; nextImage: () => void, speed: number, className: string } = {
+    index: 0,
+    speed: 5000,
+    className: 'visible',
+    images: document.querySelectorAll<HTMLImageElement>('#slideshow > img'),
+    nextImage: () => {
+      slideShow.images.forEach((img, i) =>
+        img.classList[i === slideShow.index ? 'add' : 'remove'](slideShow.className)
+      );
+
+      slideShow.index = (slideShow.index + 1) % slideShow.images.length;
+
+      setTimeout(slideShow.nextImage, slideShow.speed);
+    }
+  };
+
+  slideShow.nextImage();
 });
